2eddcc475bc3b37a4115eafbaafa444eb8e7f43d
dong706 <dong706@gmail.com>
2022-05-22 14:18:07 +0800

    屏蔽git提交&一律使用images文件夹下图片

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 hexo.extend.helper.register('cdn_css', require('./lib/helper').cdn_css);
 hexo.extend.helper.register('cdn_js', require('./lib/helper').cdn_js);
 hexo.extend.helper.register('cdn_asset', require('./lib/helper').cdn_asset);
-var cdnConfig = require('./lib/process').getCdnConfig(hexo);
+var cdnConfig = require('./lib/processNew').getCdnConfig(hexo);
 
 if (cdnConfig && 'use_cdn' in cdnConfig && cdnConfig.use_cdn) {
-  hexo.extend.filter.register('before_post_render', require('./lib/process').processPost);
+  hexo.extend.filter.register('before_post_render', require('./lib/processNew').processPost);
   hexo.extend.filter.register('before_exit',  require('./lib/generator').assetsGenerator);
   hexo.extend.filter.register('before_exit', require('./lib/generator').assetsDeployer);
 }
